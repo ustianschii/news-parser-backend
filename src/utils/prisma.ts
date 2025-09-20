5264827a284eb3df9c341df42bf3c7b45369f19e
ustianschii <ustianschii@gmail.com>
2025-09-20 11:32:30 +0300

    feat(prisma): add handler for connection/disconnection from db

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,5 +1,27 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export default prisma;
+async function main() {
+
+   const newPost = await prisma.post.create({
+        data: {
+            title: "My first post",
+            body: "Hello, Prisma + MongoDB!"
+        }
+    })
+    console.log("Created:", newPost)
+    
+  const allPosts = await prisma.post.findMany()
+  console.log(allPosts)
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
